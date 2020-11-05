d20adb5aee2e7188ad9ca8b321b2757ce86aa247
John <jonhiedarn@gmail.com>
2020-11-05 10:48:32 +0200

    Issue #17 completed.

---
+++ app/src/main/java/org/codegrinders/tresure_hunter_mobile/ActivityLogin.java
@@ -0,0 +1,74 @@
+package org.codegrinders.tresure_hunter_mobile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
+
+public class ActivityLogin extends AppCompatActivity
+{
+    Button bt_login;
+    TextView tv_register;
+    EditText etName;
+
+    AwesomeValidation awesomeValidation;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etName = findViewById(R.id.et_name);
+        bt_login = findViewById(R.id.bt_login);
+        tv_register = findViewById(R.id.tv_register);
+        tv_register.setOnClickListener(v -> openActivityRegister());
+
+        //Initialize Validation Style
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+
+        //Add Validation for Name
+        awesomeValidation.addValidation(this,R.id.et_name, RegexTemplate.NOT_EMPTY,R.string.invalid_name);
+
+        //Add Email
+        awesomeValidation.addValidation(this,R.id.et_email, Patterns.EMAIL_ADDRESS,R.string.invalid_email);
+
+        //Add Password
+        awesomeValidation.addValidation(this,R.id.et_password,".{6,}",R.string.invalid_password);
+
+        bt_login.setOnClickListener(v -> {
+            //Check Validation
+            if(awesomeValidation.validate()){
+                //On Success
+                Toast.makeText(getApplicationContext(),"Login Successfully...",Toast.LENGTH_SHORT).show();
+
+                openActivityMain();
+
+            }else{
+                Toast.makeText(getApplicationContext(),"Login Failed...",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void openActivityMain()
+    {
+        Intent intent = new Intent(this, ActivityMain.class);
+        startActivity(intent);
+    }
+
+    private void openActivityRegister()
+    {
+        Intent intent = new Intent(this,ActivityRegister.class);
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/org/codegrinders/tresure_hunter_mobile/ActivityRegister.java
@@ -0,0 +1,70 @@
+package org.codegrinders.tresure_hunter_mobile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
+
+public class ActivityRegister extends AppCompatActivity
+{
+    //initialize Variable
+    EditText etName,etEmail,etPassword,etConfirmPassword;
+    Button bt_submit;
+
+    AwesomeValidation awesomeValidation;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        //Assign Variable
+        etName = findViewById(R.id.et_name);
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
+        etConfirmPassword = findViewById(R.id.et_confirm_password);
+        bt_submit = findViewById(R.id.bt_submit);
+
+        //Initialize Validation Style
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+
+        //Add Validation for Name
+        awesomeValidation.addValidation(this,R.id.et_name, RegexTemplate.NOT_EMPTY,R.string.invalid_name);
+
+        //Add Email
+        awesomeValidation.addValidation(this,R.id.et_email, Patterns.EMAIL_ADDRESS,R.string.invalid_email);
+
+        //Add Password
+        awesomeValidation.addValidation(this,R.id.et_password,".{6,}",R.string.invalid_password);
+
+        //Add Confirm Password
+        awesomeValidation.addValidation(this,R.id.et_confirm_password,R.id.et_password,R.string.invalid_confirm_password);
+
+        bt_submit.setOnClickListener(v -> {
+            //Check Validation
+            if(awesomeValidation.validate()){
+                //On Success
+                Toast.makeText(getApplicationContext(),"Form Validate Successfully...",Toast.LENGTH_SHORT).show();
+
+                openActivityLogin();
+
+            }else{
+                Toast.makeText(getApplicationContext(),"Validation Failed...",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void openActivityLogin()
+    {
+        Intent intent = new Intent(this, ActivityLogin.class);
+        startActivity(intent);
+    }
+}
