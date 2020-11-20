2b274b375e66e522e9cb225e10d6f98922e4530a
Georgerayt <raytopoulos2@gmail.com>
2020-11-20 15:22:16 +0200

    fixed permission error, now the app gets puzzles instead of users and compares the first puzzle's answer with user input, and displays if its correct or wrong.

--- app/src/main/java/org/codegrinders/treasure_hunter_mobile/ActivityPuzzles.java
+++ app/src/main/java/org/codegrinders/treasure_hunter_mobile/ActivityPuzzles.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,6 +22,8 @@ public class ActivityPuzzles extends AppCompatActivity {
     TextView tv_question;
     EditText et_answer;
 
+    String answer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,37 +40,34 @@ public class ActivityPuzzles extends AppCompatActivity {
                 .build();
         APIService apiService = retrofit.create(APIService.class);
 
-        Call<List<Users>> call = apiService.getUsers();
-        Call<List<Puzzles>> call2 = apiService.getPuzzles();
+        Call<List<Users>> callUsers = apiService.getUsers();
+        Call<List<Puzzles>> callPuzzles = apiService.getPuzzles();
 
-        call.enqueue(new Callback<List<Users>>() {
+        callPuzzles.enqueue(new Callback<List<Puzzles>>() {
             @Override
-            public void onResponse(Call<List<Users>> call, Response<List<Users>> response) {
+            public void onResponse(Call<List<Puzzles>> call, Response<List<Puzzles>> response) {
                 if (!response.isSuccessful()) {
                     tv_question.setText("code: " + response.code());
+                    return;
                 }
-                List<Users> users = response.body();
-                for (Users user : users) {
-                    String content = "";
-                    content += "ID: " + user.getId() + "\n";
-                    content += "EMAIL: " + user.getEmail() + "\n";
-                    content += "USERNAME: " + user.getUsername() + "\n";
-                    content += "PASSWORD: " + user.getPassword() + "\n";
-                    content += "POINTS: " + user.getPoints() + "\n\n";
-
-                    tv_question.append(content);
-                }
+                List<Puzzles> puzzles = response.body();
+                answer = puzzles.get(0).getAnswer();
+                tv_question.setText(puzzles.get(0).getQuestion());
             }
 
             @Override
-            public void onFailure(Call<List<Users>> call, Throwable t) {
+            public void onFailure(Call<List<Puzzles>> call, Throwable t) {
                 tv_question.setText(t.getMessage());
             }
         });
 
-
         bt_continue.setOnClickListener(v -> {
-            //tv_question.setText("");
+            //Toast.makeText(this, "res= "+answer, Toast.LENGTH_LONG).show();
+            if(et_answer.getText().toString().equals(answer)){
+                Toast.makeText(this, "!!! CORRECT !!!", Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(this, "WRONG ANSWER :(", Toast.LENGTH_LONG).show();
+            }
         });
 
     }
