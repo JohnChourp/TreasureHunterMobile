78a4c47dd3bfb07ba4b32cee378bfe7a60e5fd43
Georgerayt <raytopoulos2@gmail.com>
2020-12-06 21:03:47 +0200

    Added more Tests for the 'isCorrect' method of RetroInstance.

--- app/src/test/java/org/codegrinders/treasure_hunter_mobile/retrofit/RetroInstanceTest.java
+++ app/src/test/java/org/codegrinders/treasure_hunter_mobile/retrofit/RetroInstanceTest.java
@@ -1,9 +1,14 @@
 package org.codegrinders.treasure_hunter_mobile.retrofit;
 
+import org.codegrinders.treasure_hunter_mobile.tables.Puzzle;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
@@ -13,6 +18,7 @@ import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -76,6 +82,48 @@ public class RetroInstanceTest {
         boolean correct = mockedInstance.isCorrect("10");
         assertTrue(correct);
     }
+
+    @Test
+    public void whenIsCorrectIsCalledWithCorrectAnswerCheckIfAnswerIsCorrect(){
+        RetroInstance retroInstance = new RetroInstance();
+        retroInstance.setPuzzles(new ArrayList<>());
+        retroInstance.getPuzzles().add(new Puzzle());
+        retroInstance.getPuzzles().get(0).setAnswer("answer1");
+        assertTrue(retroInstance.isCorrect("answer1"));
+    }
+
+    @Test
+    public void whenIsCorrectIsCalledWithWrongAnswerCheckIfAnswerIsNotCorrect(){
+        RetroInstance retroInstance = new RetroInstance();
+        retroInstance.setPuzzles(new ArrayList<>());
+        retroInstance.getPuzzles().add(new Puzzle());
+        retroInstance.getPuzzles().get(0).setAnswer("answer1");
+        assertFalse(retroInstance.isCorrect("ans"));
+    }
+
+    @Test
+    public void whenIsCorrectIsCalledASecondTimeItChecksTheNextAnswerOnThePuzzleList(){
+        RetroInstance retroInstance = new RetroInstance();
+        retroInstance.setPuzzles(new ArrayList<>());
+        retroInstance.getPuzzles().add(new Puzzle());
+        retroInstance.getPuzzles().add(new Puzzle());
+        retroInstance.getPuzzles().get(0).setAnswer("answer1");
+        retroInstance.getPuzzles().get(1).setAnswer("answer2");
+        assertTrue(retroInstance.isCorrect("answer1"));
+        assertTrue(retroInstance.isCorrect("answer2"));
+    }
+
+    @Test
+    public void whenIsCorrectIsCalledASecondTimeAndTheresOnlyOnePuzzleItReturnsFalse(){
+        RetroInstance retroInstance = new RetroInstance();
+        retroInstance.setPuzzles(new ArrayList<>());
+        retroInstance.getPuzzles().add(new Puzzle());
+        retroInstance.getPuzzles().get(0).setAnswer("answer1");
+        assertTrue(retroInstance.isCorrect("answer1"));
+        assertFalse(retroInstance.isCorrect(""));
+    }
+
+
 
     @Test
     public void usersGetRequest() {
