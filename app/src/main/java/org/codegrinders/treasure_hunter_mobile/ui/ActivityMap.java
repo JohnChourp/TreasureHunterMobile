cd298512b06ebf7f5c8dddc4bbe6ae5df345da40
Azaer <fonoman_apostolis@yahoo.com>
2020-12-11 12:22:34 +0200

    Proximity markers v2.5

--- app/src/main/java/org/codegrinders/treasure_hunter_mobile/ui/ActivityMap.java
+++ app/src/main/java/org/codegrinders/treasure_hunter_mobile/ui/ActivityMap.java
@@ -108,8 +108,7 @@ public class ActivityMap extends AppCompatActivity implements
     }
 
 
-    private void openActivityPuzzles()
-    {
+    private void openActivityPuzzles() {
         Intent intent = new Intent(this, ActivityPuzzle.class);
         startActivity(intent);
     }
@@ -117,6 +116,16 @@ public class ActivityMap extends AppCompatActivity implements
 
     private void proximityMarkers() {
         LocationManager locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         Location location = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
         final double[] longitude = {location.getLongitude()};
