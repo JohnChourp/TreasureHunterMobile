2ad107e5acec5f31faeb329d11ce4189f0fca0a0
John <jonhiedarn@gmail.com>
2021-01-01 21:13:03 +0200

    changed colors in activity setting

--- app/src/main/java/org/codegrinders/treasure_hunter_mobile/retrofit/RetroInstance.java
+++ app/src/main/java/org/codegrinders/treasure_hunter_mobile/retrofit/RetroInstance.java
@@ -15,7 +15,7 @@ public class RetroInstance {
                 .build();
 
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://10.0.2.2:8080/")
+                .baseUrl("http://192.168.2.2:8080/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(okHttpClient)
                 .build();
