20f0111a0ad832a4c748b7c7029be593284b3184
FanisCham <fchamalidis@yahoo.com>
2021-01-11 02:18:08 +0200

    Added java file for Edit Profile

---
+++ app/src/main/java/org/codegrinders/treasure_hunter_mobile/ui/ActivityEditProfile.java
@@ -0,0 +1,16 @@
+package org.codegrinders.treasure_hunter_mobile.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.codegrinders.treasure_hunter_mobile.R;
+
+public class ActivityEditProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editprofile);
+    }
+}
