1d74d466defa4f294998ed1849528885d05f7543
FanisCham <fchamalidis@yahoo.com>
2021-01-12 15:31:35 +0200

    Added activity changePassword

---
+++ app/src/main/java/org/codegrinders/treasure_hunter_mobile/ui/ActivityChangePassword.java
@@ -0,0 +1,16 @@
+package org.codegrinders.treasure_hunter_mobile.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.codegrinders.treasure_hunter_mobile.R;
+
+public class ActivityChangePassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_changepassword);
+    }
+}
