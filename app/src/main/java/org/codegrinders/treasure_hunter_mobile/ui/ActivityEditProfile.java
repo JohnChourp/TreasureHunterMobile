0ca5c271319539f69964ebccf000fac592d6695a
FanisCham <fchamalidis@yahoo.com>
2021-01-13 21:07:39 +0200

    added change password activity and some resume buttons

--- app/src/main/java/org/codegrinders/treasure_hunter_mobile/ui/ActivityEditProfile.java
+++ app/src/main/java/org/codegrinders/treasure_hunter_mobile/ui/ActivityEditProfile.java
@@ -1,16 +1,16 @@
 package org.codegrinders.treasure_hunter_mobile.ui;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import org.codegrinders.treasure_hunter_mobile.R;
 
 public class ActivityEditProfile extends AppCompatActivity {
 
-    Button bt_editEmail;
+    Button bt_editEmail, bt_editPassword, bt_goBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +18,25 @@ public class ActivityEditProfile extends AppCompatActivity {
         setContentView(R.layout.activity_editprofile);
 
         bt_editEmail = findViewById(R.id.bt_editEmail);
+        bt_editPassword = findViewById(R.id.bt_editPassword);
+        bt_goBack = findViewById(R.id.bt_goBack);
+
         bt_editEmail.setOnClickListener(v -> openActivityEditEmail());
+        bt_editPassword.setOnClickListener(v -> openActivityChangePassword());
+        bt_goBack.setOnClickListener(v -> returnOnActivityUserMenu());
     }
 
     private void openActivityEditEmail() {
         Intent intent = new Intent(this, ActivityEditEmail.class);
         startActivity(intent);
     }
-}
+
+    private void openActivityChangePassword() {
+        Intent intent = new Intent(this, ActivityChangePassword.class);
+        startActivity(intent);
+    }
+
+    private void returnOnActivityUserMenu() {
+        finish();
+    }
+    }
