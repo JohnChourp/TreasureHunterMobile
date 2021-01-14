cb6ab4d5c26cdf6d5b2c17954fcb8aacffe0fb4c
FanisCham <fchamalidis@yahoo.com>
2021-01-14 22:39:28 +0200

    Closes issue #19

--- app/src/main/java/org/codegrinders/treasure_hunter_mobile/menu/ActivityEditEmail.java
+++ app/src/main/java/org/codegrinders/treasure_hunter_mobile/menu/ActivityEditEmail.java
@@ -49,7 +49,6 @@ public class ActivityEditEmail extends AppCompatActivity {
             usersCall.oneUserGetRequest(UsersCall.user.getId());
             usersCall.setCallBack(retroCallBack);
 
-
         });
 
     }
